74b05681baaea9bfed686317ba6a54d3190047e4
juliobrandao-maker <julio.brandao@upe.br>
2025-10-28 17:08:16 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import math
+import threading
+import sys
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import funcoes_controle.py
+# --- Configurações Globais---
+STRING_CONEXAO = "udp:127.0.0.1:14550" 
+veiculo = None
+drone=STRING_CONEXÃO
+estado="iniciando"
+if estado=="inicando"
+  print("Drone sendo armado"
+  armar(drone):
