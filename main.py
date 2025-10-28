6dac818e6e87c66e193e8663f2839854afb8626d
juliobrandao-maker <julio.brandao@upe.br>
2025-10-28 17:14:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from pymavlink import mavutil
 import funcoes_controle.py
 # --- Configurações Globais---
 STRING_CONEXAO = "udp:127.0.0.1:14550" 
-veiculo = None
 drone=STRING_CONEXÃO
-estado="iniciando"
-if estado=="inicando"
-  print("Drone sendo armado"
+estado="armando"
+
+# ----Armando----
+if estado== "armando":
+  print("Drone sendo armado")
   armar(drone):
+  if drone.armed ==True:
+    estado="decolando"
+# ----Decolando----
+if estado=="decolando":
+  decolar(drone,3)
